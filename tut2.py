a63e8b04e87df4eadf348e4b3ec602d77d4f8ec3
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-11-07 18:18:01 +0530

    Bootstrap

---
+++ tut2.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+
+def hello():
+    return render_template('index.html')
+
+@app.route('/anmol')
+
+def anmol():
+    return "Hello World 2"
+
+@app.route('/about')
+
+def about():
+    name="prithesh"
+    return render_template('about.html',name=name)
+
+@app.route('/bootstrap')
+
+def bootstrap():
+    # name="prithesh"
+    return render_template('bootstrap.html')
+
+
+app.run(debug=True)
